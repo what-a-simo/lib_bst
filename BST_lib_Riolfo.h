ec2156d09f86a5444580dc8e134ac440f772fe1e
simo <simone.riolfo07@gmail.com>
2024-12-10 19:29:21 +0100

    operator overloading finished

--- BST_lib_Riolfo.h
+++ BST_lib_Riolfo.h
@@ -6,20 +6,20 @@
 using namespace std;
 
 class Node {
-private:
-    int data;
-    int count;
-    Node* left_child;
-    Node* right_child;
-public:
-    Node(int k);
-    Node* insertR(int k);
-    Node* insertI(int k);
-    bool searchR(int K);
-    bool searchI(int k);
-    void inOrder();
-    friend istream& operator>>(istream& is, Node* node);
-    friend ostream& operator<<(ostream& os, Node* node);
+    private:
+        int data;
+        int count;
+        Node* left_child;
+        Node* right_child;
+    public:
+        Node(int k);
+        Node* insertR(int k);
+        Node* insertI(int k);
+        bool searchR(int K);
+        bool searchI(int k);
+        void inOrder();
+        friend istream& operator>>(istream& is, Node* node);
+        friend ostream& operator<<(ostream& os, Node* node);
 };
 
 #endif // NODE_HPP
